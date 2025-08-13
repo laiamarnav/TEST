30cae75584a0c2ced6256786aa964787a02fb437
Laia Martínez Navarro <laia.martinezn@col.vueling.com>
2025-08-13 10:23:36 +0000

    Updated check_nugets.py

--- check_nugets.py
+++ check_nugets.py
@@ -179,7 +179,7 @@ def run_dotnet_package_check(csproj_path, check_type, blocked_packages, whitelis
                 log_summary(f"WARNING: Package '{package_name}' allowed by '*' whitelist (project/global).")
                 continue
 
-            if "-beta" in package_name:
+            if "-beta" in installed_version:
                 log(f"BETA.")
                 is_whitelisted_beta = (
                     any(fnmatch.fnmatch(package_name, wl) for wl in whitelist_for_project) or
